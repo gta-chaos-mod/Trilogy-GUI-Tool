020a51c09e2fdc3150036d6b1b47624dcafaccb8
Lordmau5 <mail@lordmau5.com>
2019-07-15 21:39:16 +0200

    Version 0.992

--- Form1.cs
+++ Form1.cs
@@ -27,7 +27,7 @@ namespace GTA_SA_Chaos
         {
             InitializeComponent();
 
-            Text = "GTA:SA Chaos v0.991";
+            Text = "GTA:SA Chaos v0.992";
             tabSettings.TabPages.Remove(tabDebug);
 
             Stopwatch = new Stopwatch();

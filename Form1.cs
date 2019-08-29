7aee2cd36d66d2c6fe991b65e368c9edabadefbc
Lordmau5 <mail@lordmau5.com>
2019-08-29 19:09:30 +0200

    Add Night Vision back into Speedrun preset

--- Form1.cs
+++ Form1.cs
@@ -535,7 +535,7 @@ namespace GTA_SA_Chaos
                 "CE1", "CE2", "CE3", "CE4", "CE5", "CE6", "CE7", "CE8", "CE9", "CE10",
                 "CE11", "CE12", "CE13", "CE14", "CE15", "CE16", "CE17", "CE18", "CE19",
                 "CE21", "CE22", "CE23", "CE24", "CE25", "CE26", "CE27", "CE28", "CE29", "CE30",
-                "CE31", "CE32", "CE33", "CE34", "CE35", "CE36", "CE37", "CE39", "CE40",
+                "CE31", "CE32", "CE33", "CE34", "CE35", "CE36", "CE37", "CE38", "CE39", "CE40",
                 "CE41", "CE42", "CE43", "CE44", "CE45", "CE46", "CE47", "CE48",
 
                 "TP1"

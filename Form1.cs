4a59368a3198accaa9605335364458b390102d76
Lordmau5 <mail@lordmau5.com>
2019-08-31 15:19:07 +0200

    1.0 Release! 🎉

--- Form1.cs
+++ Form1.cs
@@ -27,7 +27,7 @@ namespace GTA_SA_Chaos
         {
             InitializeComponent();
 
-            Text = "GTA:SA Chaos v0.999";
+            Text = "GTA:SA Chaos v1.0";
             tabSettings.TabPages.Remove(tabDebug);
 
             Stopwatch = new Stopwatch();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-GUI-tool
+# GTA Chaos Mod
+Welcome to the official GTA Chaos Mod repository!
+
+For the actual readme on this, please head on over to the [asi-script](https://github.com/gta-chaos-mod/asi-script) repository.

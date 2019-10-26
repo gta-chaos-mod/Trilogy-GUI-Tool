c485fdbbdef202cca805225fa15b2281076d457c
Bart de Bever <bartdebever112@gmail.com>
2019-10-26 21:28:42 +0200

    Cooldown time is no dynamically made

--- GtaChaos.Wpf.Core/ViewModels/CooldownViewModel.cs
+++ GtaChaos.Wpf.Core/ViewModels/CooldownViewModel.cs
@@ -1,22 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GtaChaos.Wpf.Core.ViewModels
 {
     public class CooldownViewModel
     {
+        public CooldownViewModel()
+        {
+            CooldownDictionary = new Dictionary<int, string>();
+
+            foreach (var cooldownTime in CooldownTimes.OrderBy(time => time))
+            {
+                var timeSpan = new TimeSpan(0,0,0, cooldownTime);
+                if (timeSpan.Minutes > 0)
+                {
+                    CooldownDictionary.Add(cooldownTime,
+                        $"{Math.Round(timeSpan.TotalMinutes, 2)} Minutes ({timeSpan.TotalSeconds} Seconds)");
+                    continue;
+                }
+                if (timeSpan.Seconds > 0)
+                {
+                    CooldownDictionary.Add(cooldownTime, $"{Math.Round(timeSpan.TotalSeconds, 2)} Seconds");
+                    continue;
+                }
+
+                CooldownDictionary.Add(cooldownTime, $"{timeSpan.TotalMilliseconds} Milliseconds.");
+            }
+        }
+
+        private static IEnumerable<int> CooldownTimes => new int[]
+        {
+            1,
+            10,
+            30,
+            60,
+            120,
+            200,
+            300
+        };
+
         public double SelectedCooldown { get; set; }
 
-        public Dictionary<double, string> CooldownDictionary => new Dictionary<double, string>()
-        {
-            {0.500, "500 Milliseconds" },
-            {1, "1 Second" },
-            {10, "10 Seconds" },
-            {30, "30 Seconds" },
-            {60, "1 Minute" },
-            {120, "2 Minutes" },
-            {300,"5 Minutes" }
-        };
+        public Dictionary<int, string> CooldownDictionary { get; }
     }
 }

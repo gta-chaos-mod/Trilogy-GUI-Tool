5e889d22c57c8fa73dd6a5eff119e8504faba2cb
Lordmau5 <mail@lordmau5.com>
2020-09-11 12:13:56 +0200

    v2.3.3

--- GtaSaChaos.Models/Utils/Shared.cs
+++ GtaSaChaos.Models/Utils/Shared.cs
@@ -3,7 +3,7 @@ namespace GtaChaos.Models.Utils
 {
     public static class Shared
     {
-        public static string Version = "2.3.1";
+        public static string Version = "2.3.3";
 
         public static string SelectedGame = "san_andreas";
 

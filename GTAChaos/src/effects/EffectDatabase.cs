0d41e91c0ce04ec03e4bcbb1f405e40711849530
Lordmau5 <mail@lordmau5.com>
2022-07-02 12:38:58 +0200

    Add effect `Screensaver HUD`

--- GTAChaos/src/effects/EffectDatabase.cs
+++ GTAChaos/src/effects/EffectDatabase.cs
@@ -287,6 +287,7 @@ namespace GTAChaos.Effects
                 AddEffect(new FunctionEffect(Category.CustomEffects, "Custom Textures", "CustomTextures", "textures_custom"));
                 AddEffect(new FunctionEffect(Category.CustomEffects, "Delayed Controls", "WhatsWrongWithThisKeyboard", "delayed_controls", -1, 1.0f));
                 AddEffect(new FakeCrashEffect("Game Crash", "TooManyModsInstalled"));
+                AddEffect(new FunctionEffect(Category.CustomEffects, "Screensaver HUD", "ScreensaverHUD", "screensaver_hud"));
                 AddEffect(new FunctionEffect(Category.CustomEffects, "It's Morbin Time", "ItsMorbinTime", "textures_its_morbin_time"));
                 AddEffect(new FunctionEffect(Category.CustomEffects, "Low LOD Vehicles", "AtariVehicles", "low_lod_vehicles"));
                 //AddEffect(new FunctionEffect(Category.CustomEffects, "Greyscale Screen", "GreyscaleScreen", "greyscale_screen", -1, 1.0f)); // Greyscale Screen
